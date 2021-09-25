b891ca71c16dbecc616eb9f666d454c4dbfaf31d
Aakash <85122418+Okush19@users.noreply.github.com>
2021-09-25 14:05:34 +0530

    Update ownertools.py

--- handlers/ownertools.py
+++ handlers/ownertools.py
@@ -34,7 +34,7 @@ async def botstats(_, message: Message):
     disk_usage = psutil.disk_usage('/').percent
     total_users = await db.total_users_count()
     await message.reply_text(
-        text=f"**📊 stats Of @{BOT_USERNAME}** \n\n**🤖 bot version:** `v6.5` \n\n**🙎🏼 users:** \n » **users on pm:** `{total_users}` \n\n**💾 disk usage,** \n » **disk space:** `{total}` \n » **used:** `{used}({disk_usage}%)` \n » **free:** `{free}` \n\n**🎛 hardware usage,** \n » **CPU usage:** `{cpu_usage}%` \n » **RAM usage:** `{ram_usage}%`",
+        text=f"**📊 Stats Of @{BOT_USERNAME}** \n\n**🤖 Bot Version:** `v6.5` \n\n**🙎🏼 Users:** \n » **User's on pm:** `{total_users}` \n\n**💾 Disk usage,** \n » **Disk space:** `{total}` \n » **used:** `{used}({disk_usage}%)` \n » **free:** `{free}` \n\n**🎛 hardware usage,** \n » **CPU usage:** `{cpu_usage}%` \n » **RAM usage:** `{ram_usage}%`",
         parse_mode="Markdown",
         quote=True
     )
